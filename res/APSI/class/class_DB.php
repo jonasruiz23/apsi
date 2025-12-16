4c7157f53187c7de305b769ef75f92b6eeaeed19
Jonas Ruiz <mdqb2415@gmail.com>
2025-12-16 16:18:44 +0800

    dd
    dd

---
+++ res/APSI/class/class_DB.php
@@ -0,0 +1,55 @@
+<?php
+
+session_start();
+error_reporting(0);
+
+
+date_default_timezone_set('Asia/Manila');
+
+
+	$servername = "localhost";
+$username = "u901683710_apsiserveruser";
+$password = "AEEj7Wxf@MA7yJq";
+$dbname = "u901683710_apsiserver";
+
+/*
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "apsiserver";
+
+*/
+// Create connection
+$con = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
+}
+
+
+
+
+
+
+
+// DB credentials.
+define('DB_HOST',$servername);
+define('DB_USER',$username);
+define('DB_PASS',$password );
+define('DB_NAME',$dbname);
+// Establish database connection.
+try
+{
+$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+}
+catch (PDOException $e)
+{
+exit("Error: " . $e->getMessage());
+}
+
+
+
+// echo accountlogs("Do","Form","Description","$No",$userid);
+
+?>
+
